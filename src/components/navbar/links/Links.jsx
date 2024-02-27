a3533ece2cc2ad453b53aa29a2900c8e3dd097c6
tomprnd <155766870+tomprnd@users.noreply.github.com>
2024-02-27 14:54:21 +0100

    ADD

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,35 +1,49 @@
+"use client"
 import Link from "next/link";
 import styles from "./links.module.css"
+import NavLink from "./navLink/navLink";
+import { useState } from "react";
+
+const links = [
+    {
+        title: "Homepage",
+        path: "/",
+    },
+
+    {
+        title: "About",
+        path: "/about",
+    },
+
+    {
+        title: "Contact",
+        path: "/contact",
+    },
+
+    {
+        title: "Blog",
+        path: "/blog",
+    },
+];
 
 const Links = () => {
-
-    const links = [
-        {
-            title: "Homepage",
-            path: "/",
-        },
-
-        {
-            title: "About",
-            path: "/about",
-        },
-
-        {
-            title: "Contact",
-            path: "/contact",
-        },
-
-        {
-            title: "Blog",
-            path: "/blog",
-        },
-    ];
+    const [open,setOpen] = useState(false)
 
   return (
-    <div className={styles.links}>
-        {links.map((link=>(
-            <NaVLink item={link} key={link.title}/> 
-        )))}
+    <div className={styles.container}>
+        <div className={styles.links}>
+            {links.map((link=>(
+                <NavLink item={link} key={link.title}/> 
+            )))}
+        </div>
+        <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>Menu</button>
+        {
+            open && <div className={styles.mobileLinks}>
+            {links.map((link=>(
+                    <NavLink item={link} key={link.title}/>
+            )))}
+        </div>
+        }
     </div>    
   )
 };
